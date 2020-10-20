3c3a9d967f5f0882b689b9e78930238ea44e0e20
josemiguelpeinado <josemiguel.peinado@mad.vass.es>
2020-10-20 11:55:36 +0200

    Resto de archivos

---
+++ src/main/java/com/jmpm/microservice/SpringBootTestingV2Application.java
@@ -0,0 +1,13 @@
+package com.jmpm.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmpm/microservice/SpringBootTestingV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmpm.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
